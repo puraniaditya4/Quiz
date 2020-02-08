6d1787f073fc094cdf80a122a8a05553441b10a2
mrsinghamritpalnitj <49278124+mrsinghamritpalnitj@users.noreply.github.com>
2020-02-07 22:36:44 -0500

    Add files via upload

---
+++ Main5Activity.java
@@ -0,0 +1,25 @@
+package com.example.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class Main5Activity extends AppCompatActivity {
+  Sensor s1;
+  SensorManager sm;
+  TextView t1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+        findViewById(R.id.textView3);
+        
+
+    }
+}
